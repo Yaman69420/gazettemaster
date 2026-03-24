d6459789d964d9f4ae40006e661a7c530e9999a9
Yaman Terkawi <Yaman.terkawi.yt@gmail.com>
2026-03-24 13:51:43 +0100

    about en contact pagina aangemaakt in de frontend stijl

---
+++ resources/views/frontend/about.blade.php
@@ -0,0 +1,12 @@
+<x-frontend.shell title="Over ons">
+    <div class="container" style="padding: 60px 15px;">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <h1 class="mb-4">Over de Gazette</h1>
+                <p class="lead">De Gazette is een onafhankelijk nieuwsplatform dat kwalitatieve artikels brengt over technologie, Laravel en moderne webontwikkeling.</p>
+                <p>Onze redactie bestaat uit ervaren schrijvers en developers die dagelijks werken met de nieuwste technologieën. We delen onze kennis graag via uitgebreide tutorials, opiniestukken en nieuws.</p>
+                <p>Heb je een vraag of wil je samenwerken? Neem gerust contact op via de <a href="{{ route('contact') }}">contactpagina</a>.</p>
+            </div>
+        </div>
+    </div>
+</x-frontend.shell>
